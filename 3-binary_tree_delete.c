cb1c0022e4ee3e1b7f1b732c1f8fc0eb24ea65e6
dominic6809 <muuomusyoki018@gmail.com>
2024-06-04 02:13:53 -0700

    delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function that Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * Return: void
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return;
+
+    /* Recursively delete left subtree */
+    binary_tree_delete(tree->left);
+
+    /* Recursively delete right subtree */
+    binary_tree_delete(tree->right);
+
+    /* Delete the current node */
+    free(tree);
+}
