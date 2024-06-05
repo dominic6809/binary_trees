84765fae28c0afd952e2ceec4ac02a9d7d36ce96
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-05 08:40:38 +0300

    function that goes through a binary tree using level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * binary_tree_levelorder - function that Traverses a binary tree
  * using level-order traversal
  *
- * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node.
+ * @tree: A Pointer to the root node of the tree to traverse
+ * @func: A Pointer to a function to call for each node.
  * The value in the node must be passed as a parameter to this function.
  *
  * Return: void
