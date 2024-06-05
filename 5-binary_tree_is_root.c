ea50e69d3941fcd442995fde8a28342614766655
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-05 08:48:14 +0300

    checks if a given node is a root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_is_root - function that checks if a given node is a root
- * @node: Pointer to the node to check
+ * @node: A Pointer to the node to check
  *
  * Return: 1 if node is a root, otherwise 0
  */
