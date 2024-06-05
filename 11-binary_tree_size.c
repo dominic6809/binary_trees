8ffad7576b633051a06da2ae63ebb04c95629394
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-05 08:45:03 +0300

    measures the size of a binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -4,7 +4,7 @@
  * binary_tree_size - function that measures the size of a binary tree
  * @tree: Pointer to the root node of the tree to measure the size
  *
- * Return: Size of the tree, 0 if tree is NULL
+ * Return: The Size of the tree, 0 if tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
