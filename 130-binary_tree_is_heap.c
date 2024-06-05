6b5d0f0962db81f1142b730c9d7e3055a71d10f5
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-05 08:29:46 +0300

    function that checks if a binary tree is a valid Max Binary Heap

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -4,7 +4,7 @@
  * binary_tree_size - Measures the size of a binary tree.
  * @tree: Pointer to the root node of the tree to measure the size.
  *
- * Return: Size of the tree.
+ * Return: Size of the tree on success.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
