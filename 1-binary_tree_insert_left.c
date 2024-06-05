b691e8b9995ff0808a92aac5e375a5714dce6a2b
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-05 08:50:32 +0300

    inserts a node as the left-child of another node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_insert_left - Inserts a node as the left-child of another node
  * @parent: Pointer to the node to insert the left-child in
- * @value: Value to store in the new node
+ * @value: The Value to store in the new node
  *
  * Return: Pointer to the created node, or NULL on failure
  * or if parent is NULL
